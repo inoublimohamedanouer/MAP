2e824dd23684029ddc803c371fa6c2ed88a340be
anouer <inoublimohamedanouer123@gmail.com>
2024-02-01 22:24:46 +0100

    first commit

---
+++ load.js
@@ -0,0 +1,444 @@
+const gouvernorats = new Array(
+    {
+        gouvernorat : "Bizerte",
+        image : "images/Bizerte.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Bizerte",
+        delegation : "Bizerte Nord,Bizerte Sud",
+        code_postal : 7000,
+        gentile : "Bizertin",
+        population : 136917, // hab 2014
+        densite : 4027, // hab/km²
+        coordonnees : "37° 16′ nord, 9° 52′ est",
+        altitude : 33, //m
+        superficie : 34, //km²
+    },
+    {
+        gouvernorat : "Béja",
+        image : "images/Béja.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Béja",
+        delegation : "Béja Nord,Béja Sud",
+        code_postal : 9000,
+        gentile : "Béjaois",
+        population : 62303, // hab 2014
+        densite : 4774, // hab/km²
+        coordonnees : "36° 43′ 30″ nord, 9° 10′ 55″ est",
+        altitude : 222, //m
+        superficie : 13.05, //km²
+    },
+    {
+        gouvernorat : "Ariana",
+        image : "images/Ariana.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Ariana",
+        delegation : "Ariana Ville",
+        code_postal : null,
+        gentile : "Arianais",
+        population : 114486, // hab 2014
+        densite : 636, // hab/km²
+        coordonnees : "36° 51′ nord, 10° 12′ est",
+        altitude : 25, //m
+        superficie : 18, //km²
+    },
+    {
+        gouvernorat : "Manouba",
+        image : "images/Manouba.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Manouba",
+        delegation : "Manouba",
+        code_postal : null,
+        gentile : null,
+        population : 32029, // hab 2014
+        densite : 3701, // hab/km²
+        coordonnees : "36° 48′ 26″ nord, 10° 06′ 03″ est",
+        altitude : null, //m
+        superficie : 8.654, //km²
+    },
+    {
+        gouvernorat : "BenArous",
+        image : "images/BenArous.JPG",
+        pays : "Tunisie",
+        gouvernorats : "Ben Arous",
+        delegation : "Ben Arous",
+        code_postal : null,
+        gentile : null,
+        population : 88322, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 44′ nord, 10° 13′ est",
+        altitude : 44, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Tunis",
+        image : "images/Tunis.JPG",
+        pays : "Tunisie",
+        gouvernorats : "Tunis",
+        delegation : "Bab El Bhar,Bab Souika,Cité El Khadra,Djebel Jelloud,El Kabaria,El Menzah,El Omrane,El Omrane supérieur,El Ouardia,Ettahrir,Ezzouhour,Hraïria,Médina,Séjoumi,Sidi El Béchir",
+        code_postal : 1000,
+        gentile : "Tunisois",
+        population : 638845, // hab 2014
+        densite : 3004, // hab/km²
+        coordonnees : "36° 47′ 51″ nord, 10° 09′ 57″ est",
+        altitude : 40, //m
+        superficie : 21.263, //km²
+    },
+    {
+        gouvernorat : "Jendouba",
+        image : "images/Jendouba.JPG",
+        pays : "Tunisie",
+        gouvernorats : "Jendouba",
+        delegation : "Jendouba Sud,Jendouba Nord",
+        code_postal : 8100,
+        gentile : "Jendoubien",
+        population : 45431, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 30′ 02″ nord, 8° 46′ 51″ est",
+        altitude : 141, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Nabeul",
+        image : "images/Nabeul.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Nabeul",
+        delegation : "Nabeul",
+        code_postal : 8000,
+        gentile : "Nabeulien",
+        population : 70437, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 27′ nord, 10° 44′ est",
+        altitude : 4, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Zaghouan",
+        image : "images/Zaghouan.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Zaghouan",
+        delegation : "Zaghouan",
+        code_postal : 1100,
+        gentile : "Zaghouanais",
+        population : 20798, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 24′ nord, 10° 09′ est",
+        altitude : 174, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Siliana",
+        image : "images/Siliana.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Siliana",
+        delegation : "Siliana Nord,Siliana Sud",
+        code_postal : 6100,
+        gentile : "Silianais",
+        population : 31251, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 04′ 55″ nord, 9° 22′ 29″ est",
+        altitude : 428, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Kef",
+        image : "images/Kef.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Kef",
+        delegation : "Kef Est,Kef Ouest",
+        code_postal : 7100,
+        gentile : "Keffois",
+        population : 54690, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "36° 11′ 10″ nord, 8° 42′ 00″ est",
+        altitude : 627, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Sousse",
+        image : "images/Sousse.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Sousse",
+        delegation : "Sousse Jawhara,Sousse Médina,Sousse Riadh,Sousse Sidi Abdelhamid",
+        code_postal : 4000,
+        gentile : "Soussien, Soussienne",
+        population : 221530, // hab 2014
+        densite : 4923, // hab/km²
+        coordonnees : "35° 49′ 34″ nord, 10° 38′ 24″ est",
+        altitude : 25, //m
+        superficie : 45, //km²
+    },
+    {
+        gouvernorat : "Kairouan",
+        image : "images/Kairouan.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Kairouan",
+        delegation : "Kairouan Nord,Kairouan Sud",
+        code_postal : 3100,
+        gentile : "Kairouanais",
+        population : 139070, // hab 2014
+        densite : 2045, // hab/km²
+        coordonnees : "35° 40′ nord, 10° 05′ est",
+        altitude : 68, //m
+        superficie : 6802, //km²
+    },
+    {
+        gouvernorat : "Monastir",
+        image : "images/Monastir.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Monastir",
+        delegation : "Monastir",
+        code_postal : 5000,
+        gentile : "Monastirien",
+        population : 104535, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "35° 46′ nord, 10° 49′ est",
+        altitude : 20, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Mahdia",
+        image : "images/Mahdia.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Mahdia",
+        delegation : "Mahdia",
+        code_postal : 5100,
+        gentile : "Mahdois",
+        population : 51833, // hab 2014
+        densite : 80, // hab/km²
+        coordonnees : "35° 30′ nord, 11° 04′ est",
+        altitude : 13, //m
+        superficie : 645, //km²
+    },
+    {
+        gouvernorat : "Kasserine",
+        image : "images/Kasserine.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Kasserine",
+        delegation : "Kasserine Nord,Kasserine Sud,Ezzouhour",
+        code_postal : 1200,
+        gentile : "Kasserinois",
+        population : 83534, // hab 2014
+        densite : 6961, // hab/km²
+        coordonnees : "35° 10′ nord, 8° 50′ est",
+        altitude : 656, //m
+        superficie : 12, //km²
+    },
+    {
+        gouvernorat : "SidiBouzid",
+        image : "images/SidiBouzid.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Sidi Bouzid",
+        delegation : "Sidi Bouzid Est,Sidi Bouzid Ouest",
+        code_postal : 9100,
+        gentile : "Bouzidien",
+        population : 48284, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "35° 02′ nord, 9° 30′ est",
+        altitude : 327, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Sfax",
+        image : "images/Sfax.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Sfax",
+        delegation : "Sfax Est,Sfax Ouest,Sfax Ville",
+        code_postal : 3000,
+        gentile : "Sfaxien",
+        population : 272801, // hab 2014
+        densite : 4871, // hab/km²
+        coordonnees : "34° 44′ nord, 10° 46′ est",
+        altitude : 13, //m
+        superficie : 56, //km²
+    },
+    {
+        gouvernorat : "Gafsa",
+        image : "images/Gafsa.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Gafsa",
+        delegation : "Gafsa Nord,Gafsa Sud",
+        code_postal : null,
+        gentile : "Gafsien",
+        population : 95242, // hab 2014
+        densite : 2116, // hab/km²
+        coordonnees : "34° 25′ nord, 8° 47′ est",
+        altitude : 298, //m
+        superficie : 45, //km²
+    },
+    {
+        gouvernorat : "Gabes",
+        image : "images/Gabes.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Gabes",
+        delegation : "Gabes Medina,Gabes Ouest,Gabes Sud",
+        code_postal : 6000,
+        gentile : "Gabésien",
+        population : 130984, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "33° 53′ nord, 10° 07′ est",
+        altitude : 1, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Tozeur",
+        image : "images/Tozeur.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Tozeur",
+        delegation : "Tozeur",
+        code_postal : 2200,
+        gentile : "Tozeurois",
+        population : 37365, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "33° 55′ nord, 8° 08′ est",
+        altitude : 42, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Kebili",
+        image : "images/Kebili.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Kebili",
+        delegation : "Kebili Nord,Kebili Sud",
+        code_postal : 4200,
+        gentile : "Kébilien",
+        population : 20623, // hab 2014
+        densite : null, // hab/km²
+        coordonnees : "33° 42′ 18″ nord, 8° 57′ 54″ est",
+        altitude : 49, //m
+        superficie : null, //km²
+    },
+    {
+        gouvernorat : "Medenine",
+        image : "images/Medenine.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Medenine",
+        delegation : "Medenine Nord,Medenine Sud",
+        code_postal : 4100,
+        gentile : "Médeninois",
+        population : 71406, // hab 2014
+        densite : 1298, // hab/km²
+        coordonnees : "33° 20′ 42″ nord, 10° 29′ 24″ est",
+        altitude : 81, //m
+        superficie : 55, //km²
+    },
+    {
+        gouvernorat : "Tataouine",
+        image : "images/Tataouine.jpg",
+        pays : "Tunisie",
+        gouvernorats : "Tataouine",
+        delegation : "Tataouine Nord,Tataouine Sud",
+        code_postal : 3200,
+        gentile : "Tataouinois",
+        population : 66924, // hab 2014
+        densite : 1593, // hab/km²
+        coordonnees : "32° 55′ 40″ nord, 10° 26′ 57″ est",
+        altitude : 247, //m
+        superficie : 42, //km²
+    }
+)
+
+
+const load = () =>{
+    const url = new URLSearchParams(window.location.search)
+    const gouv = url.get('value')
+    for(i = 0 ; i<gouvernorats.length; i++){
+        if(gouvernorats[i].gouvernorat == gouv){
+            document.write(
+                `
+                <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: black;
+            color:white;
+        }
+        h2{
+            background-color: red;
+            padding: 20px;
+            margin: 20px;
+            text-align: center;
+        }
+        .container{
+            display: flex;
+            justify-content: start;
+            flex-direction: column;
+            border: 2px white solid;
+            width: 40%;
+            min-width: 600px;
+        }
+        table{
+            margin-left: 20px;
+            padding-bottom: 20px;
+        }
+        #image{
+            width: 500;
+            height: 300px;
+            margin: 20px;
+        }
+    </style>
+    <div class="container">
+        <div class="header"><h2>${gouvernorats[i].gouvernorats}</h2></div>
+        <img src="${gouvernorats[i].image}" id="image">
+        <div class="header"><h2>Administration</h2></div>
+        <table>
+            <tr>
+                <th>Pays</th>
+                <td>${gouvernorats[i].pays}</td>
+            </tr>
+            <tr>
+                <th>Gouvernorat</th>
+                <td>${gouvernorats[i].gouvernorats}</td>
+            </tr>
+            <tr>
+                <th>Délégation(s)</th>
+                <td>${gouvernorats[i].delegation}</td>
+            </tr>
+            <tr>
+                <th>Code Postal</th>
+                <td>${gouvernorats[i].code_postal}</td>
+            </tr>
+        </table>
+        <div class="header"><h2>Démocraphie</h2></div>
+        <table>
+            <tr>
+                <th>Gentilé</th>
+                <td>${gouvernorats[i].gentile}</td>
+            </tr>
+            <tr>
+                <th>Population (hab)</th>
+                <td>${gouvernorats[i].population}</td>
+            </tr>
+            <tr>
+                <th>Densité (hab/km²)</th>
+                <td>${gouvernorats[i].densite}</td>
+            </tr>
+        </table>
+        <div class="header"><h2>Géographie</h2></div>
+        <table>
+            <tr>
+                <th>Coordonnées</th>
+                <td>${gouvernorats[i].coordonnees}</td>
+            </tr>
+            <tr>
+                <th>Altitude (m)</th>
+                <td>${gouvernorats[i].altitude}</td>
+            </tr>
+            <tr>
+                <th>Superficie (km²)</th>
+                <td>${gouvernorats[i].superficie}</td>
+            </tr>
+        </table>
+    </div>
+                `
+            )
+            break
+        }
+    }
+}
+
+load();
